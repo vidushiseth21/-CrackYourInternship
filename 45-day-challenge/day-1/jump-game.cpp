9ec2aea1cc990620309312a80b84c8e17ede0ef0
vidushiseth21 <sethvidushi750@gmail.com>
2024-07-16 21:21:56 +0530

    day2

---
+++ 45-day-challenge/day-1/jump-game.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int maxi=0;
+        for(int i=0;i<n;i++)
+        {
+            if(maxi<i)
+             return false;
+            maxi=max(maxi,i+nums[i]);
+            if(maxi>=n-1)
+             return true;
+        }
+        return false;
+    }
+};
