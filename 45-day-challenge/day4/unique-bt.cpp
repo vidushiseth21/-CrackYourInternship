8c1537a689e7dccba57332f894502a8d7c57fb96
vidushiseth21 <sethvidushi750@gmail.com>
2024-07-19 19:03:53 +0530

    day5

---
+++ 45-day-challenge/day4/unique-bt.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    
+    int solve(int n)
+    {
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        //number of node
+        for(int i=2;i<=n;i++)
+        {
+            //root node
+            for(int j=1;j<=i;j++)
+            {
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+    int numTrees(int n) {
+        
+        return solve(n);
+    }
+};
